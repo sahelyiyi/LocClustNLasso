01e9242f278a7ea49ef79d952ef2ad7a52e271da
Sahelyiyi <sahel@Sahelyiyis-MacBook-Pro.local>
2020-10-12 14:11:57 +0300

    add logistic regression

---
+++ log_reg_lasso/log_reg_sbm.py
@@ -0,0 +1,60 @@
+import numpy as np
+from stochastic_block_model import get_B_and_weight_vec
+from log_reg_lasso.main import *
+
+
+def threshold(val):
+    return 1/(1+np.exp(val))
+
+
+def run_reg_sbm(K, lambda_lasso, m, n, N1=150, N2=150, M=0.2):
+    # TODO fix this
+    m = 1
+    M=1.0
+    B, weight_vec = get_B_and_weight_vec(N1, N2, pout=0.001, mu_in=40, mu_out=10)
+    E, N = B.shape
+
+    X = []
+    for i in range(N):
+        # x, y = np.meshgrid(np.linspace(-1, 1, n), np.linspace(-1, 1, m))
+        # d = np.sqrt(x * x + y * y)
+        sigma, mu = 1.0, 0.0
+        # g = np.exp(-((d - mu) ** 2 / (2.0 * sigma ** 2)))
+        g = np.random.normal(mu, sigma, (m, n))
+        X.append(g)
+    X = np.array(X)
+
+    W1 = np.random.random(n)
+    W2 = np.random.normal(5, 2, n)
+    print (W1, W2)
+
+    Y = []
+    for i in range(N):
+        if i < N1:
+            p = np.dot(X[i], W1)
+        else:
+            p = np.dot(X[i], W2)
+        random_variable = np.random.uniform()
+        Y.append(1 if random_variable > threshold(p)[0] else 0)
+    Y = np.array(Y)
+
+    return run(K, B, weight_vec, Y, X, lambda_lasso, method='norm', M=M)
+
+# run_reg_sbm(K=2000, lambda_lasso=0.4, m=5, n=15) -> (0.07543958130185237, 0.4327561139685135, 0.470397364281547)
+# run_reg_sbm(K=1000, lambda_lasso=0.4, m=5, n=15) -> (0.09223178656091131, 0.4220348484968601, 0.5021987887453047)
+# run_reg_sbm(K=500, lambda_lasso=0.4, m=5, n=15)  -> (0.10498491617601496, 0.4088509340133603, 0.4751957719337881)
+
+# run_reg_sbm(K=1000, lambda_lasso=0.1, m=5, n=15) -> (0.08507156724382309, 0.3709281732014657, 0.4482499135199026)
+# run_reg_sbm(K=500, lambda_lasso=0.1, m=5, n=15)  -> (0.09318609467039467, 0.40197666378593205, 0.4755215981200701)
+
+# run_reg_sbm(K=500, lambda_lasso=0.07, m=5, n=15)  -> (0.09318609467039467, 0.40197666378593205, 0.4755215981200701)
+
+
+# run_reg_sbm(K=500, lambda_lasso=0.05, m=5, n=2)  -> (0.047971014813955964, 0.428114030234606, 0.48165557665190534)
+
+# run_reg_sbm(K=1000, lambda_lasso=0.01, m=5, n=2) -> (0.0022115219836184824, 0.4703629298378227, 0.5129913488269128)
+# run_reg_sbm(K=500, lambda_lasso=0.01, m=5, n=2)  -> (0.0014434393839232477, 0.39116590556765635, 0.4373967984554611)
+# run_reg_sbm(K=300, lambda_lasso=0.01, m=5, n=2)  -> (0.003002048073650676, 0.3836411265938509, 0.43169274260015283)
+
+# run_reg_sbm(K=500, lambda_lasso=0.005, m=5, n=2)  -> (0.00033012642876345126, 0.4301791931028151, 0.47592633684825336)
+
---
+++ log_reg_lasso/main.py
@@ -0,0 +1,130 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import r2_score, mean_squared_log_error, mean_squared_error, mean_absolute_error
+import random
+
+
+def nmse_func(Y, Y_pred):
+    MSE = mean_squared_error(Y, Y_pred)
+    NMSE = MSE / np.square(Y).mean()
+    return NMSE
+
+
+def run(K, B, weight_vec, Y, X, lambda_lasso=0.1, method=None, M=0.2):
+    functions = {
+        'mean_squared_log_error': mean_squared_log_error,
+        'mean_squared_error': mean_squared_error,
+        'normalized_mean_squared_error': nmse_func,
+        'mean_absolute_error': mean_absolute_error
+    }
+
+    if method == 'log':
+        default_score_func = mean_squared_log_error
+    elif method == 'norm':
+        default_score_func = nmse_func
+    else:
+        default_score_func = mean_squared_error
+
+    Sigma = np.diag(1./(2*weight_vec))
+
+    D = B
+
+    Gamma_vec = (1.0/(np.sum(abs(B), 0))).T
+    Gamma = np.diag(Gamma_vec)
+
+    if np.linalg.norm(np.dot(Sigma**0.5, D).dot(Gamma**0.5), 2) > 1:
+        print ('product norm', np.linalg.norm(np.dot(Sigma**0.5, D).dot(Gamma**0.5), 2))
+        # raise Exception('higher than 1')
+
+    E, N = B.shape
+    m, n = X[0].shape
+    samplingset = random.sample([i for i in range(N)], k=int(M * N))
+    not_samplingset = [i for i in range(N) if i not in samplingset]
+    new_w = np.array([np.zeros(n) for i in range(N)])
+    prev_w = np.array([np.zeros(n) for i in range(N)])
+    new_u = np.array([np.zeros(n) for i in range(E)])
+
+    MTX2 = {}
+    BETA = {}
+    for i in samplingset:
+        MTX2[i] = (Gamma_vec[i]/m * X[i]).T
+        BETA[i] = Gamma_vec[i] * np.power(np.linalg.norm(X[i]), 2)
+
+    limit = np.array([np.zeros(n) for i in range(E)])
+    for i in range(n):
+        limit[:, i] = lambda_lasso*weight_vec
+    iteration_scores = []
+    for iterk in range(K):
+        if iterk % 100 == 0:
+            print ('iter:', iterk)
+        prev_w = np.copy(new_w)
+
+        hat_w = new_w - np.dot(Gamma, np.dot(D.T, new_u))  # could  be negative
+        new_w = np.copy(hat_w)
+
+        for i in range(N):
+            if i in samplingset:
+                phi_i = lambda vu: hat_w[i] + np.dot(MTX2[i], (1 / (1 + np.exp(-np.dot(X[i], vu))) - Y[i]))
+                try:
+                    lp = int(np.ceil(2 * np.log(iterk) / np.log(1 / BETA[i])))
+                except:
+                    lp = 10
+                lp = max(lp, 10)
+                for j in range(lp):
+                    new_w[i] = phi_i(new_w[i])
+            else:
+                new_w[i] = hat_w[i]
+
+        tilde_w = 2 * new_w - prev_w
+        new_u = new_u + np.dot(Sigma, np.dot(D, tilde_w))  # chould be negative
+
+        normalized_u = np.where(abs(new_u) >= limit)
+        new_u[normalized_u] = limit[normalized_u] * new_u[normalized_u] / abs(new_u[normalized_u])
+
+    Y_pred = []
+    for i in range(N):
+        if np.dot(X[i], new_w[i]) > 0:
+            Y_pred.append(0)
+        else:
+            Y_pred.append(1)
+    Y_pred = np.array(Y_pred)
+    # iteration_scores.append(default_score_func(Y, Y_pred))
+    print (len(np.where(Y!=Y_pred)[0])/N)
+    # if np.max(abs(new_w - prev_w)) > 5 * 1e-3:
+    # print (np.max(abs(new_w - prev_w)))
+    #     # raise Exception('not converged')
+    #
+    # our_score = {}
+    # for score_func_name, score_func in functions.items():
+    #     our_score[score_func_name] = score_func(Y, Y_pred)
+    #
+    # x = np.mean(X, 1)
+    # y = np.mean(Y, 1)
+    # model = LinearRegression().fit(x[samplingset], y[samplingset])
+    # pred_y = model.predict(x)
+    # linear_regression_score = {}
+    # for score_func_name, score_func in functions.items():
+    #     linear_regression_score[score_func_name] = score_func(y, pred_y)
+    #
+    # y = Y.reshape(-1, 1)
+    # x = X.reshape(-1, n)
+    # decision_tree_samplingset = []
+    # for item in samplingset:
+    #     for i in range(m):
+    #         decision_tree_samplingset.append(m*item+i)
+    #
+    # max_depth = 5
+    # regressor = DecisionTreeRegressor(max_depth=max_depth)
+    # regressor.fit(x[decision_tree_samplingset], y[decision_tree_samplingset])
+    # pred_y = regressor.predict(x)
+    # decision_tree_score = {}
+    # for score_func_name, score_func in functions.items():
+    #     decision_tree_score[score_func_name] = score_func(y, pred_y)
+    #
+    # # decision_tree_non_samplingset = [i for i in range(len(x)) if i not in decision_tree_samplingset]
+    # # print ('\tdecision tree max_depth:', max_depth,
+    # #        '\n\t\ttrain error:', default_score_func(y[decision_tree_samplingset], regressor.predict(x[decision_tree_samplingset])),
+    # #        '\n\t\ttest error:', default_score_func(y[decision_tree_non_samplingset], regressor.predict(x[decision_tree_non_samplingset])))
+    #
+    # return iteration_scores, our_score, linear_regression_score, decision_tree_score
